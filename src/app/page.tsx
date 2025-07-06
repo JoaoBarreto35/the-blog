6b6aea4c89dbeb035bf1252b773650426b5416f9
João Barreto <barretojoao440@gmail.com>
2025-07-06 14:27:15 -0300

    Construido primeiro container geral, e repositorio dos posts para poder adaptar-se em diferentes fontes de dados - no momento em JSON

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
-export default function HomePage() {
-  return <h1>Olá de dentro de page.tsx</h1>;
+import { Container } from "@/components/Container";
+import PostsList from "@/components/postsList";
+import SpinLoader from "@/components/SpinLoader";
+
+import { Suspense } from "react";
+
+export default async function HomePage() {
+  return (
+    <Container>
+      <header>
+        <h1 className="text-6xl font-bold py-8 text-center">HEADER</h1>
+      </header>
+      <Suspense fallback={<SpinLoader />}>
+        <PostsList />
+      </Suspense>
+
+      <footer className="text-6xl font-bold py-8 text-center">footer</footer>
+    </Container>
+  );
 }
