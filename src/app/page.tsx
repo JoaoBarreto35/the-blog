4d24918ba8e10b8a3e6f2fcc96a19ef0ec871195
João Barreto <barretojoao440@gmail.com>
2025-07-07 21:53:21 -0300

    Configurando previamente o layout.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import { Container } from "@/components/Container";
-import { Header } from "@/components/Header";
 import { PostFeatured } from "@/components/PostFeatured";
 import PostsList from "@/components/postsList";
 import SpinLoader from "@/components/SpinLoader";
@@ -8,17 +6,13 @@ import { Suspense } from "react";
 
 export default async function HomePage() {
   return (
-    <Container>
-      <Header />
-
+    <>
       <Suspense>
         <PostFeatured />
       </Suspense>
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
-
-      <footer className="text-6xl font-bold py-8 text-center">footer</footer>
-    </Container>
+    </>
   );
 }
